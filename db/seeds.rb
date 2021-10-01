e8513ef85b8e7df039f66e2990195167499e7273
Sonia <serrano_sonia@hotmail.com>
2021-09-30 20:39:29 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ prepadvarray = []
 verbasocprep = []
 
 #Split verbs and prepositions/adv in different arrays
-19.times do |i|
+somephrasalverbs.length.times do |i|
     verbprep =  somephrasalverbs[i].split('%20')
     verbarray[i] = verbprep[0]
     prepadvarray[i] = verbprep[1]
@@ -62,39 +62,79 @@ verbfilt.length.times do |i|
         phrasal_id = phrasal_id_obj[0].id
         if((result[0]['meanings'][0]['definitions'].length >= 3))
             defmean0 = result[0]['meanings'][0]['definitions'][0]['definition']
-            meaning1 = Meaning.create!(definition: defmean0,  phrasal_id: phrasal_id) 
+            meaning0 = Meaning.create!(definition: defmean0,  phrasal_id: phrasal_id) 
+            meanexample0 = result[0]['meanings'][0]['definitions'][0]['example']
+            meansynonyms0 = result[0]['meanings'][0]['definitions'][0]['synonyms']
+            meananthonyms0 = result[0]['meanings'][0]['definitions'][0]['antonyms']
+            meanbody0 = Meanbody.create!(synonyms: meansynonyms0, anthonyms:meananthonyms0, 
+                                        example:meanexample0, meaning_id:meaning0.id)
 
             defmean1 = result[0]['meanings'][0]['definitions'][1]['definition']
-            meaning2 = Meaning.create!(definition: defmean1,  phrasal_id: phrasal_id) 
- 
+            meaning1 = Meaning.create!(definition: defmean1,  phrasal_id: phrasal_id) 
+            meanexample1 = result[0]['meanings'][0]['definitions'][1]['example']
+            meansynonyms1 = result[0]['meanings'][0]['definitions'][1]['synonyms']
+            meananthonyms1 = result[0]['meanings'][0]['definitions'][1]['antonyms']
+            meanbody1 = Meanbody.create!(synonyms: meansynonyms1, anthonyms:meananthonyms1, 
+                                        example:meanexample1, meaning_id:meaning1.id)
+     
             defmean2 = result[0]['meanings'][0]['definitions'][2]['definition']
-            meaning3 = Meaning.create!(definition: defmean2, phrasal_id: phrasal_id) 
-          
+            meaning2 = Meaning.create!(definition: defmean2, phrasal_id: phrasal_id) 
+            meanexample2 = result[0]['meanings'][0]['definitions'][2]['example']
+            meansynonyms2 = result[0]['meanings'][0]['definitions'][2]['synonyms']
+            meananthonyms2 = result[0]['meanings'][0]['definitions'][2]['antonyms']
+            meanbody2 = Meanbody.create!(synonyms: meansynonyms2, anthonyms:meananthonyms2, 
+                                        example:meanexample2, meaning_id:meaning2.id)
            
-             
         else if ((result[0]['meanings'][0]['definitions'].length >= 2))
 
             defmean0 = result[0]['meanings'][0]['definitions'][0]['definition']
-            meaning1 = Meaning.create!(definition: defmean0,  phrasal_id: phrasal_id) 
+            meaning0 = Meaning.create!(definition: defmean0,  phrasal_id: phrasal_id)
+            meanexample0 = result[0]['meanings'][0]['definitions'][0]['example']
+            meansynonyms0 = result[0]['meanings'][0]['definitions'][0]['synonyms']
+            meananthonyms0 = result[0]['meanings'][0]['definitions'][0]['antonyms']
+            meanbody0 = Meanbody.create!(synonyms: meansynonyms0, anthonyms:meananthonyms0, 
+                                        example:meanexample0, meaning_id:meaning0.id)
+
           
             defmean1 = result[0]['meanings'][0]['definitions'][1]['definition']
-            meaning2 = Meaning.create!(definition: defmean1,  phrasal_id: phrasal_id) 
+            meaning1 = Meaning.create!(definition: defmean1,  phrasal_id: phrasal_id)
+            meanexample1 = result[0]['meanings'][0]['definitions'][1]['example']
+            meansynonyms1 = result[0]['meanings'][0]['definitions'][1]['synonyms']
+            meananthonyms1 = result[0]['meanings'][0]['definitions'][1]['antonyms']
+            meanbody1 = Meanbody.create!(synonyms: meansynonyms1, anthonyms:meananthonyms1, 
+                                        example:meanexample1, meaning_id:meaning1.id)
 
-        
             else
                 defmean0 = result[0]['meanings'][0]['definitions'][0]['definition']
-                meaning1 = Meaning.create!(definition: defmean0,  phrasal_id: phrasal_id) 
-             
-        
-            end
-    
-            
+                meaning0 = Meaning.create!(definition: defmean0,  phrasal_id: phrasal_id)
+                meanexample0 = result[0]['meanings'][0]['definitions'][0]['example']
+                meansynonyms0 = result[0]['meanings'][0]['definitions'][0]['synonyms']
+                meananthonyms0 = result[0]['meanings'][0]['definitions'][0]['antonyms']
+                meanbody0 = Meanbody.create!(synonyms: meansynonyms0, anthonyms:meananthonyms0, 
+                                            example:meanexample0, meaning_id:meaning0.id)
+            end    
         end 
-    
-    end   
-  
-    
+    end    
 end
+
+#I should seed at least 20 phrases
+get_id = (Verb.where("name = 'get'"))[0].id
+come_id =(Verb.where("name = 'come'"))[0].id
+take_id =(Verb.where("name = 'take'"))[0].id
+keep_id =(Verb.where("name = 'keep'"))[0].id
+call_id =(Verb.where("name = 'call'"))[0].id
+
+
+phrase1 = Phrase.create!(
+            question: "And that is precisely the point Paul is trying to __________",
+            answer: "get across",
+            verb_id: get_id
+)
+phrase2 = Phrase.create!(
+            question: "You can __________ your coat and stay a while. ",
+            answer: "take off",
+            verb_id: take_id
+)
 
 
 
