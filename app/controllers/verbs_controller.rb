03f1be38d479c1081146572825e76e6ebf4e611a
Sonia <serrano_sonia@hotmail.com>
2021-10-04 06:43:32 -0400

    new route

--- app/controllers/verbs_controller.rb
+++ app/controllers/verbs_controller.rb
@@ -4,4 +4,19 @@ class VerbsController < ApplicationController
         render json: verbs
     end
 
+    def phrasalverb
+        verbname = find_verb
+        verbnameprepadv= Prepadv.includes(:verbs).where(verbs: {name: verbname.name})
+        render json: verbnameprepadv
+    end
+
+
+    private
+
+    def find_verb
+        Verb.find(params[:id])
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users
   resources :prepadvs
   resources :verbs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/verbs/:id/prepadvs', to: 'verbs#phrasalverb'
 end
+
